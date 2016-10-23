cdcfeaff3e9e58636abd1879c66811e7e7844378
dherault <david.herault@edge-factory.com>
2016-10-23 19:01:27 +0200

    create plugin

---
+++ spec/hit-stats-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import KeyCountStatsView from '../lib/hit-stats-view';
+
+describe('KeyCountStatsView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
